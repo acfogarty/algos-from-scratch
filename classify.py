fa18c72f796548ce4ef0d271d7b8ea641ad396a6
Aoife Fogarty <fogarty@mpip-mainz.mpg.de>
2016-09-04 20:42:12 +0200

    calculates conditional probabilities P(f|l)

--- classify.py
+++ classify.py
@@ -1,13 +1,21 @@
 import io
 import sys
-sys.path.insert(1,'/Library/Python/2.7/site-packages')
 import numpy as np
+import pandas as pd
+sys.path.insert(1,'/Library/Python/2.7/site-packages')
 import nltk
 from nltk.corpus import stopwords
 import string
 
+#Naive Bayes
+# l is member of labels
+# f is member of features
+# P(l|features) = P(l,features)/P(features)
+# P(l,features) = P(l)*Product[P(f|l)]
+
 # global variables
 stopwords = stopwords.words('english')
+stopwords += ['.',',',';','?','!','-',':','',"n't","'d","'re","'s","'m"]
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 vocabSize = 20 #no. of words used as features
 
@@ -20,14 +28,24 @@ def main():
   filenames, sampleLabels = getDataList(dataDir, labelledDataFile)
 
   #get vocabulary matrix (dim: nsamples*vocabSize)
-  sampleVocabMatrix = getVocabMatrix(filenames)
+  vocabulary, sampleVocabMatrix = getVocabMatrix(filenames)
   print '# Created vocabulary matrix of dimensions ',sampleVocabMatrix.shape
   print sampleVocabMatrix
+
+  sampleDataFrame = pd.DataFrame(data = sampleVocabMatrix,
+                                 index = filenames,
+                                 columns = vocabulary)
+  sampleDataFrame['label'] = sampleLabels
+  print sampleDataFrame.info()
 
   #get probabilities P(l) for all l in labels
   labelProbabilities = getLabelProbabilities(sampleLabels)
   print '# label probabilities ',labelProbabilities
 
+  labels = labelProbabilities.keys() #ordered set
+
+  #get conditional probabilities P(f|l) for all l in labels and f in features
+  featureLabelCondProbabilites = getFeatureLabelCondProbabilites(sampleDataFrame,labels)
 
 def getFeatures(words,vocabulary):
   '''check which elements of the vocabulary are in the list words'''
@@ -60,8 +78,8 @@ def getVocabMatrix(filenames):
     f = io.open(filename,'r',encoding="utf-8")
     raw = f.read()
     f.close()
+    raw = ''.join(i for i in raw if ord(i)<128) #clean non-ascii characters
     processed = raw.lower()
-    #processed = [s.translate(remove_punctuation_map) for s in processed]
     tokens = nltk.word_tokenize(processed)
     tokensNoStopwords = [w for w in tokens if w not in stopwords]
   
@@ -79,7 +97,7 @@ def getVocabMatrix(filenames):
     vocabMatrix.append(features)
   vocabMatrix = np.asarray(vocabMatrix)
 
-  return vocabMatrix
+  return vocabulary, vocabMatrix
 
 def getLabelProbabilities(sampleLabels):
   '''calculates percentage for each label in list of all labels'''
@@ -91,7 +109,24 @@ def getLabelProbabilities(sampleLabels):
     labelProbabilities[l] = prob
   return labelProbabilities
 
-    
+#def getFeatureLabelCondProbabilites(sampleVocabMatrix,sampleLabels,labels):
+def getFeatureLabelCondProbabilites(sampleDataFrame,labels):
+  '''calculate conditional probabilities of features on labels:
+     P(f|l) for all f and l'''
+  vocabulary = sampleDataFrame.columns.values.tolist()[:-1] #TODO do the columns stay in order, so that the last one is 'label'?
+  featureLabelCondProbabilites = pd.DataFrame(index=labels,columns=vocabulary)
+  for label in labels:
+    #get samples labelled "label"
+    samplesInClass = sampleDataFrame[sampleDataFrame['label'] == label]
+    countLabel = float(samplesInClass.shape[0])
+    #print 'count(l) ',label,countLabel
+    for word in vocabulary:
+      #count how many of the samples labelled "label" contain the feature "word"
+      countFeature = float(samplesInClass[samplesInClass[word]].shape[0])
+      #print 'count(f|l) ',word,label,countFeature
+      print 'P(f|l) ',word,label,countFeature/countLabel #TODO add smoothing
+      featureLabelCondProbabilites[word][label] = countFeature/countLabel #P(f|l)
+  return featureLabelCondProbabilites
  
 if __name__ == '__main__':
   main() 
