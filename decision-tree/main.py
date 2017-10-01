ae7b18c8bc2b6494cbce53d2ed3756a1d65e31b6
Aoife Fogarty <afogarty1000@yahoo.com>
2017-10-01 12:10:26 +0200

    add gini impurity calc

--- decision-tree/main.py
+++ decision-tree/main.py
@@ -26,19 +26,41 @@ def calc_gini_impurity(nodes=None):
     chosen element would be incorrectly identified, i.e.
     meausure of misclassification)
     nodes should be dict of datasets identified by node_id, e.g.
-    {0: {'X': X_0, 'Y': Y_0}, 1: {'X': X_1, 'Y': Y_1}}'''
+    {0: {'X': X_0, 'Y': Y_0}, 1: {'X': X_1, 'Y': Y_1}}
+    gini_impurity for a node = sum over classes (p*(1-p)) =
+    1 - (sum over classes
+    '''
     classes = [0, 1]  # TODOO don't hardcode, detect at root of tree
     n_classes = len(classes)
+    gini_impurity = 0.0
+    n_total_samples = 0.0
 
     for node_id in nodes.keys():
+        gini_imp_node = 0.0  # impurity for this node
         Y = nodes[node_id]['Y']
-        n_samples = len(Y)
-        counts = dict(zip(np.unique(Y, return_counts=True)))
+        n_samples = float(len(Y))
+        n_total_samples += n_samples
+
+        # get dict of number of times each class appears in this node
+        values, counts = np.unique(Y, return_counts=True)
+        counts = dict(zip(list(values), list(counts)))
+
         for cclass in classes:
             try:
-                prob = counts[cclass]/n_samples
-            except KeyError:
+                prob = float(counts[cclass])/n_samples
+            except KeyError:  # class in not present in node at all
                 prob = 0.0
+            gini_imp_node += prob * prob
+            print('In node {}, class {} occurs with frequency {}'.format(node_id, cclass, prob))
+        gini_imp_node = 1.0 - gini_imp_node
+        print('Node {}, gini impurity {}'.format(Y, gini_imp_node))
+
+        # weight by number of samples in this node
+        gini_impurity += gini_imp_node * n_samples
+
+    # normalise weights
+    gini_impurity /= n_total_samples
+
     return gini_impurity
 
 
@@ -58,7 +80,7 @@ def make_binary_split(i_feature=None, split_value=None, X=None, Y=None):
     X_right = X[~mask]  # <= split_value
     Y_right = Y[~mask]
 
-    return {'left': {'X': X_left, 'Y': Y_left}, 'right': {'X': X_right, 'Y': Y_right}}
+    return {0: {'X': X_left, 'Y': Y_left}, 1: {'X': X_right, 'Y': Y_right}}
 
 
 def get_best_split(X=None, Y=None):
@@ -66,8 +88,11 @@ def get_best_split(X=None, Y=None):
     n_features = X.shape[1]
     best_gini_impurity = 1000
     for i_feature in range(n_features):
+        # get all unique values this feature takes in the data
         unique_values = set(X[:, i_feature])
+        # test a split on every possible split value
         for split_value in unique_values:
+            print('Testing split on value {} of feature {}'.format(split_value, i_feature))
             nodes = make_binary_split(i_feature, split_value, X, Y)
             gini_impurity = calc_gini_impurity(nodes)
             if gini_impurity < best_gini_impurity:
