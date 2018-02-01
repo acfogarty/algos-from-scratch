37b9b4a8758b3d6886b48e2086a3e08e723cb242
Aoife Fogarty <afogarty1000@yahoo.com>
2018-02-01 21:53:43 +0100

    neural network forward prop

---
+++ neural-network/main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import neural_network
+import sys
+sys.path.append('../common-functions')
+import common_fns
+
+# neural network from scratch
+# one hidden layer
+# activation function: tanh
+# loss function: cross-entropy loss
+
+filename = '../data/test-data-classification-smoking.csv'
+target = 'hospitalised'
+test_fraction = 0.25
+n_nodes_per_hidden_layer = [10, 7]
+n_hidden_layers = len(n_nodes_per_hidden_layer)
+lambda_l = 0.1  # regularisation hyperparameter
+alpha = 0.1  # learning rate
+
+# load dataset
+X, Y, X_feature_names = common_fns.get_data(filename=filename, target=target)
+X = X[:,[0,1]] # XXXX 2 features
+
+# encode Y
+Y = common_fns.one_hot_encoder(Y)
+
+# random split into test and train
+X_train, Y_train, X_test, Y_test = common_fns.split_train_test(X, Y, test_fraction=test_fraction)
+
+n_samples = len(Y_train)
+n_features = X.shape[1]
+n_classes = len(np.unique(Y))
+print('n_samples', n_samples)
+print('n_features', n_features)
+print('input matrix shape', X.shape)
+print('n_classes', n_classes)
+
+# initialise model
+nn = neural_network.neural_network(n_features, n_classes, n_nodes_per_hidden_layer)
+nn.initialize_weights()
+
+# fit network on train set
+
+# make predictions on test set
+Y_predict = nn.predict(X[:3])
+print(X[:3], Y_predict)
---
+++ neural-network/neural_network.py
@@ -0,0 +1,76 @@
+import numpy as np
+import sys
+sys.path.append('../common-functions')
+from common_fns import tanh, softmax
+
+
+class neural_network:
+
+    def __init__(self, n_features, n_classes, n_nodes_per_hidden_layer, activation_fn=None, output_activation_fn=None):
+
+        self.n_features = n_features
+        self.n_classes = n_classes
+
+        self.n_nodes_per_layer = [n_features]
+        for n in n_nodes_per_hidden_layer:
+            self.n_nodes_per_layer.append(n)
+        self.n_nodes_per_layer.append(n_classes)
+        self.n_layers = len(self.n_nodes_per_layer)
+
+        if activation_fn is None:
+            self.activation_fn = tanh
+        else:
+            self.activation_fn = activation_fn
+
+        if output_activation_fn is None:
+            self.output_activation_fn = softmax
+        else:
+            self.output_activation_fn = output_activation_fn
+
+        self.weights = []
+
+        print('initialised a neural network with {} classes, {} features'.format(self.n_classes, self.n_features))
+        print('and {} layers with nodes per layer: {}'.format(self.n_layers, self.n_nodes_per_layer))
+
+    def forward_propogate_layer(self, a, W, activation_fn):
+        '''a: np.array with dimension n_nodes_prev_layer * n_samples
+        W: weights, np.array with dimensions n_nodes * n_nodes_prev_layer
+        '''
+        print('a',a)
+        print('W',W)
+        z = np.dot(W, a)
+        print('z',z)
+        return activation_fn(z)
+
+    def predict(self, X):
+        '''
+        X: np.array with dimensions n_samples * n_features
+        returns: np.array with dimensions n_samples * n_classes XXXX'''
+        A = X.transpose()
+
+        for W in self.weights[:-1]:
+            # add bias node to A (add row of 1s at start of matrix)
+            bias = np.ones((1, A.shape[1]))
+            A = np.vstack((bias, A))
+            # get node outputs
+            A = self.forward_propogate_layer(A, W, self.activation_fn)
+
+        bias = np.ones((1, A.shape[1]))
+        A = np.vstack((bias, A))
+        Y = self.forward_propogate_layer(A, self.weights[-1], self.output_activation_fn)
+
+        return Y
+
+    def initialize_weights(self):
+        # TODOO add different initialisation options
+        print('Initialising weights with dimensions:')
+        for i in range(self.n_layers-1):
+            # self.n_nodes_per_layer[i] + 1 because of bias term
+            W = np.random.random((self.n_nodes_per_layer[i+1], self.n_nodes_per_layer[i] + 1))
+            W *= 2.0
+            W -= 1.0
+            self.weights.append(W)
+            print('layer {}: {}'.format(i, W.shape))
+            print(W)
+
+
