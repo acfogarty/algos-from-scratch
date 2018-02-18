9867ccc118a3ba5114753c70a92a0ff198aa735a
Aoife Fogarty <afogarty1000@yahoo.com>
2018-02-18 23:56:29 +0100

    cross validation and learning curve

--- common_functions/common_fns.py
+++ common_functions/common_fns.py
@@ -200,43 +200,53 @@ def hardmax(Y):
     return np.apply_along_axis(lambda x: np.where(x == x.max(), 1, 0), axis, Y)
 
 
-def accuracy_onehot(Y, Y_true):
+def accuracy_onehot(Y_true, Y_predict):
     '''
     calculate accuracy = (true positives + true negatives) / (total population)
     for two one-hot-encoded arrays
+    if Y_predict contains probabilities instead of 1s and 0s, hardmax is applied first
     input:
-        Y, Y_true: array of dimension n_samples*n_classes containing integers 1, 0 (one-hot-encoding)
+        Y_true, Y_predict: array of dimension n_samples*n_classes containing integers 1, 0 (one-hot-encoding) or probabilities
     output: float
     '''
-    if Y.shape != Y_true.shape:
-        raise Exception('in accuracy Y shape = {} and Y_true shape = {}'.format(Y.shape, Y_true.shape))
 
-    n_samples = Y.shape[0]
+    if Y_predict.shape != Y_true.shape:
+        raise Exception('in accuracy Y_predict shape = {} and Y_true shape = {}'.format(Y_predict.shape, Y_true.shape))
+
+    # detect whether Y_predict contains probabilities instead of one-hot encoding, and convert if necessary
+    # (detect if at least one element is not equal to 0 or 1)
+    if not np.all((Y_predict == 1) | (Y_predict == 0)):
+        Y_predict = hardmax(Y_predict)
+
+    Y_true = Y_true.astype(np.int64)
+    Y_predict = Y_predict.astype(np.int64)
+
+    n_samples = Y_predict.shape[0]
     axis = 1
-    matches = np.apply_along_axis(np.all, axis, (Y == Y_true))
+    matches = np.apply_along_axis(np.all, axis, (Y_predict == Y_true))
     n_matches = matches.sum()
 
     return float(n_matches) / float(n_samples)
 
 
-def confusion_matrix(Y, Y_true, print_matrix=False):
+def confusion_matrix(Y_true, Y_predict, print_matrix=False):
     '''calculate confusion matrix for two label-encoded arrays
     assumes classes are integers starting from 0
     actual class: rows
     predicted class: columns
     input:
-        Y, Y_true: array of length n_samples with classes encoded as integers
+        Y_true, Y_predict: array of length n_samples with classes encoded as integers
     output:
         array of dimension n_classes * n_classes
     '''
-    Y = Y.astype(np.int64)
+    Y_predict = Y_predict.astype(np.int64)
     Y_true = Y_true.astype(np.int64)
 
-    n_classes = max(np.max(Y), np.max(Y_true)) + 1
+    n_classes = max(np.max(Y_predict), np.max(Y_true)) + 1
 
     matrix = np.zeros((n_classes, n_classes))
 
-    for y_i, y_true_i in zip(Y, Y_true):
+    for y_i, y_true_i in zip(Y_predict, Y_true):
         matrix[y_true_i][y_i] += 1
 
     if print_matrix:
@@ -271,3 +281,113 @@ def calculate_scores_from_confusion_matrix(matrix):
     scores['recall'] = np.diagonal(matrix) / np.sum(matrix, axis=1)
 
     return scores
+
+
+def cross_validation_scores(model, X, Y, n_fold, score_fn, shuffle=True, train_subset_size=None):
+    '''
+    inputs:
+        model: class with functions .fit() and .predict()
+        X: np.array with dimensions n_samples * n_features
+        Y: np.array with dimensions n_samples * n_classes
+        n_fold: number of CV folds, int
+        score_fn: function that takes arguments (Y_true, Y_predict)
+    if train_subset_size != None, only train on a subset of that size of the 
+    training set (for constructing learning curves). The size of the test set
+    is not affected
+    '''
+
+    n_samples = Y.shape[0]
+    n_samples_per_fold = n_samples / n_fold
+
+    # shuffle both arrays using same permutation
+    if shuffle:
+        p = np.random.permutation(len(Y))
+        X, Y = X[p], Y[p]
+
+    training_scores = np.zeros((n_fold))
+    test_scores = np.zeros((n_fold))
+
+    for i in range(n_fold):
+
+        # split into n_fold folds
+        indices = np.arange(n_samples)
+        test_mask = ((indices >= (i * n_samples_per_fold)) & (indices < ((i + 1) * n_samples_per_fold)))
+        X_train, Y_train = X[~test_mask], Y[~test_mask]
+        X_test, Y_test = X[test_mask], Y[test_mask]
+
+        if train_subset_size:
+            # only take subset of training set (will be the same points for each call unless shuffle==True)
+            X_train, Y_train = X[:train_subset_size], Y[:train_subset_size]
+
+        # fit network on train set
+        model.fit(X_train, Y_train)
+
+        # predict on train
+        Y_predict = model.predict(X_train, transpose_Y=True)
+        training_scores[i] = score_fn(Y_true=Y_train, Y_predict=Y_predict)
+
+        # predict on test
+        Y_predict = model.predict(X_test, transpose_Y=True)
+        test_scores[i] = score_fn(Y_true=Y_test, Y_predict=Y_predict)
+
+    return training_scores, test_scores
+
+
+def learning_curve(model, X, Y, score_fn, cv_n_fold, train_sizes):
+    '''
+    if some integers in train_sizes are greater than the max possible number with the given sample size and cv_n_fold, they will simply be ignored
+    train_sizes: list of int
+    cv_n_fold: int
+    score_fn: function that takes arguments (Y_true, Y_predict)
+    '''
+
+    n_samples = Y.shape[0]
+    # max number of training set samples we can have with cv_n_fold-CV
+    n_max_samples_train = int(float(n_samples) / cv_n_fold * (cv_n_fold - 1))
+
+    training_scores = []
+    test_scores = []
+    trainsetsize_xtics = []
+
+    # cross validation for different training set sizes (test set size is always the same)
+    for train_subset_size in train_sizes:
+
+        if train_subset_size > n_max_samples_train:
+            continue
+    
+        training_scores_c, test_scores_c = cross_validation_scores(model, X, Y, n_fold=cv_n_fold, score_fn=score_fn, shuffle=True, train_subset_size=train_subset_size)
+
+        training_scores.append(training_scores_c)
+        test_scores.append(test_scores_c)
+        trainsetsize_xtics.append(train_subset_size)
+
+    np.asarray(training_scores)
+    np.asarray(test_scores)
+    np.asarray(trainsetsize_xtics)
+
+    return trainsetsize_xtics, training_scores, test_scores
+
+
+def print_model_test(model, X, Y, score_fn, oh_enc, l_enc):
+    Y_predict = model.predict(X, transpose_Y=True)
+    # print('Y_predict')
+    # print(Y_predict.shape)
+    # print('Y')
+    # print(Y.shape)
+    loss = score_fn(Y_true=Y, Y_predict=Y_predict)
+    print('score', loss)
+    # convert probabilities to 0 and 1 (max prob = 1)
+    Y_predict_hard = hardmax(Y_predict)
+    # print('Y_predict_hard')
+    # print(Y_predict_hard)
+    Y_decoded = oh_enc.decode(Y)
+    Y_predict_decoded = oh_enc.decode(Y_predict_hard)
+    print('Y_decoded')
+    print(Y_decoded)
+    print('Y_predict_decoded')
+    print(Y_predict_decoded)
+    cm = confusion_matrix(Y=Y_predict_decoded, Y_true=Y_decoded, print_matrix=True)
+    l_enc.print_decoder()
+    scores = calculate_scores_from_confusion_matrix(cm)
+    for k in scores.keys():
+        print(k, scores[k])
